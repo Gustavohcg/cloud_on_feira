69191860c427c2ba66c2a3120e30655b5f81e69b
Gustavohcg <gustavo.carvalhal.guerreiro@gmail.com>
2023-06-12 15:42:30 -0300

    feito

--- lib/pages/dispositivos/dispositivos.dart
+++ lib/pages/dispositivos/dispositivos.dart
@@ -165,6 +165,7 @@ class _DispositivosPageState<T> extends State {
                           ),
                           children: <Widget>[
                             ListView.builder(
+                              physics: const NeverScrollableScrollPhysics(),
                               shrinkWrap: true,
                               itemCount: devices.length,
                               itemBuilder: (context, index) {
